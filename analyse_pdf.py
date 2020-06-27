b771eda65f92063825ba22a5bce8930f9379e216
olliematthews <ojm1@btinternet.com>
2020-06-27 15:56:22 +0100

    Run on a single pdf

--- analyse_pdf.py
+++ analyse_pdf.py
@@ -8,94 +8,92 @@ from tabula import read_pdf
 import pandas as pd
 import csv
 
-df = pd.read_csv('../extract-pdfs/ISINs.csv')
 
-for fund_number, fund_id in enumerate(df['Fund IDs']):
-    # Save file
-    csv_file = fund_id + '.csv'
-    print('Analysing ' + str(fund_id))
-    ISIN = df[df['Fund IDs'] == fund_id].iloc[0]['ISINs']
+# Save file
+csv_file = 'save_file.csv'
+ISIN = NA
+
+pages = layout_scanner.get_pages('orcadia_pdf.pdf')
+dfs = []
+table_pages = []
+# Find the pages with "statement of investments and other net assets" is mentioned
+for page_number, page in enumerate(pages):
+    if 'Statement of investments and other net assets' in ''.join(page) and not 'table of contents' in ''.join(page).lower():
+        table_pages.append(page)
+
+rows = []
+head = ['ISIN', 'Sub-Fund', 'Asset Type', 'Country', 'Description', 'Currency',	'Quantity',	'Market value (note 2)', '% of net assets']
+
+with open(csv_file,'w+', newline = '') as file:
+    writer = csv.writer(file)
+    writer.writerow(head)
+
+flag = True
+
+for page in table_pages:
+    # First we reformat the page into the rows we want
+    lines = []
+    for l in page:
+        l = l.split('--columnbreak')
         
-    pages = layout_scanner.get_pages('../extract-pdfs/pdf_downloads/' + str(fund_id) + '.pdf')
-    dfs = []
-    table_pages = []
-    for page_number, page in enumerate(pages):
-        if 'Statement of investments and other net assets' in ''.join(page) and not 'table of contents' in ''.join(page).lower():
-            table_pages.append(page)
-    
-    rows = []
-    head = ['ISIN', 'Sub-Fund', 'Asset Type', 'Country', 'Description', 'Currency',	'Quantity',	'Market value (note 2)', '% of net assets']
-    
-    with open(csv_file,'w+', newline = '') as file:
-        writer = csv.writer(file)
-        writer.writerow(head)
-    
-    flag = True
-    
-    for page in table_pages:
-        # First we reformat the page into the rows we want
-        lines = []
-        for l in page:
-            l = l.split('--columnbreak')
-            
-            # Get rid of empty rows
-            if all(all(char == ' ' for char in entry) for entry in l):
-                continue
-            
-            # Ignore these lines
-            if 'TRANSFERABLE SECURITIES ADMITTED' in l[0]:
-                continue
-            # Get rid of rows which do not start with text
-            try:
-                float(l[0].replace(',',''))
-                continue
-            except:
-                pass
-                    
-            # End of table
-            if 'The accompanying notes' in l[0]:
-                break
-            lines.append(l)
-            
-        split_title = [string.strip(' ') for string in lines[0][0].split('-')]
-        fund = split_title[0]
-        subfund = '-'.join(split_title[1:])
-        description = lines[1][0]
-        head = ['ISIN', 'Sub-Fund', 'Asset Type', 'Country']
-        head.extend(lines[2])
+        # Get rid of empty rows
+        if all(all(char == ' ' for char in entry) for entry in l):
+            continue
         
-        for line in lines[3:]:
-            if len(line) == 3:
-                # These are usually 'total' rows. We ignore the results but note there will be a change in asset type after
-                if 'TOTAL' in line[0]:
-                    flag = True
-                continue
-            
-            elif len(line) == 1:
-                if flag:
-                    # flag indicates a change in asset type
-                    flag = False
-                    asset_type = line[0].strip(' ')
-                else:
-                    # Else there is a country change
-                    country = line[0].strip(' ')
-                continue
+        # Ignore these lines
+        if 'TRANSFERABLE SECURITIES ADMITTED' in l[0]:
+            continue
+        # Get rid of rows which do not start with text
+        try:
+            float(l[0].replace(',',''))
+            continue
+        except:
+            pass
+                
+        # End of table
+        if 'The accompanying notes' in l[0]:
+            break
+        lines.append(l)
+        
+    split_title = [string.strip(' ') for string in lines[0][0].split('-')]
+    fund = split_title[0]
+    subfund = '-'.join(split_title[1:])
+    description = lines[1][0]
+    head = ['ISIN', 'Sub-Fund', 'Asset Type', 'Country']
+    head.extend(lines[2])
+    
+    for line in lines[3:]:
+        if len(line) == 3:
+            # These are usually 'total' rows. We ignore the results but note there will be a change in asset type after
+            if 'TOTAL' in line[0]:
+                flag = True
+            continue
+        
+        elif len(line) == 1:
+            if flag:
+                # flag indicates a change in asset type
+                flag = False
+                asset_type = line[0].strip(' ')
             else:
-                # Else, we have a valid row
-                if len(line) == 6:
-                    # This is for bonds, which have two columns at the beginning. We merge these.
-                    l = [' - '.join(line[:2])]
-                    l.extend(line[2:])
-                    line = l
-                    
-                row = [ISIN, subfund, asset_type, country]
-                row.extend(line)
-                rows.append(row) 
+                # Else there is a country change
+                country = line[0].strip(' ')
+            continue
+        else:
+            # Else, we have a valid row
+            if len(line) == 6:
+                # This is for bonds, which have two columns at the beginning. We merge these.
+                l = [' - '.join(line[:2])]
+                l.extend(line[2:])
+                line = l
+                
+            row = [ISIN, subfund, asset_type, country]
+            row.extend(line)
+            rows.append(row) 
+
     
+with open(csv_file,'a', newline = '') as file:
+    writer = csv.writer(file)
+    for row in rows:
+        writer.writerow(row)
         
-    with open(csv_file,'a', newline = '') as file:
-        writer = csv.writer(file)
-        for row in rows:
-            writer.writerow(row)
-            
-            
+        
